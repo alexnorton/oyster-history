dae5dd8de48ef548454426de5aea779c29216b1d
Alex Norton <alexnorton@fastmail.com>
2018-06-23 22:37:37 +0100

    Transform rows and handle early morning times

--- lambda/handler.py
+++ lambda/handler.py
@@ -3,7 +3,7 @@ from email.parser import Parser
 import email.policy
 from io import StringIO
 from csv import DictReader
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 
 s3 = boto3.resource("s3")
@@ -57,10 +57,35 @@ def parse_csv(csv):
 
 
 def transform_row(row):
-    start_date = timezone("Europe/London").localize(
-        datetime.strptime(
-            "{} {}".format(row["Date"], row["Start Time"]), "%d-%b-%Y %H:%M"
-        )
+    row["Start Time"], row["End Time"] = parse_datetimes(
+        row["Date"], row["Start Time"], row["End Time"]
     )
 
-    return {"start_date": start_date.isoformat()}
+    row["Charge"] = float(row["Charge"]) if row["Charge"] else None
+    row["Credit"] = float(row["Credit"]) if row["Credit"] else None
+
+    row["Balance"] = float(row["Balance"])
+
+    row["Note"] = row["Note"] if row["Note"] else None
+
+    del row["Date"]
+
+    return row
+
+
+def parse_datetimes(date, start_time, end_time):
+    start = parse_datetime(date, start_time)
+
+    end = parse_datetime(date, end_time) if end_time else None
+
+    if start.hour < 4 or start.hour == 4 and start.minute < 30:
+        start = start + timedelta(days=1)
+        end = end + timedelta(days=1) if end else None
+
+    return start.isoformat(), end.isoformat() if end else None
+
+
+def parse_datetime(date, time):
+    return timezone("Europe/London").localize(
+        datetime.strptime("{} {}".format(date, time), "%d-%b-%Y %H:%M")
+    )
--- lambda/test_handler.py
+++ lambda/test_handler.py
@@ -28,4 +28,35 @@ def test_transform_row():
 
     transformed = handler.transform_row(row)
 
-    print(transformed)
+    assert "Date" not in transformed
+    assert transformed["Start Time"] == "2017-12-03T17:08:00+00:00"
+    assert transformed["End Time"] == "2017-12-03T17:20:00+00:00"
+    assert (
+        transformed["Journey/Action"] == "Shepherd's Bush (Central line) to Marble Arch"
+    )
+    assert transformed["Charge"] == 1.6
+    assert transformed["Credit"] is None
+    assert transformed["Balance"] == 18.6
+    assert transformed["Note"] is None
+
+
+def test_parse_datetimes():
+    assert handler.parse_datetimes("03-Dec-2017", "17:08", "17:20") == (
+        "2017-12-03T17:08:00+00:00",
+        "2017-12-03T17:20:00+00:00",
+    )
+
+    assert handler.parse_datetimes("01-Dec-2017", "03:44", "03:53") == (
+        "2017-12-02T03:44:00+00:00",
+        "2017-12-02T03:53:00+00:00",
+    )
+
+    assert handler.parse_datetimes("23-Jun-2018", "13:47", "14:15") == (
+        "2018-06-23T13:47:00+01:00",
+        "2018-06-23T14:15:00+01:00",
+    )
+
+    assert handler.parse_datetimes("17-Jun-2018", "23:43", "") == (
+        "2018-06-17T23:43:00+01:00",
+        None,
+    )
